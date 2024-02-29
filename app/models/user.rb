c7613e850bf1ff06058ea9444295ad18eba386fe
diegodias <diasdsdiego@gmail.com>
2024-02-29 08:46:05 -0300

    user_module: insert new routes to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :api
 
-  validates :name, :email, presence: true 
+  validates :email, presence: true 
 end
