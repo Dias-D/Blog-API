eccc11c1ebf8cec8cccc42b02fe2744f7f0fda09
diegodias <diasdsdiego@gmail.com>
2024-03-08 18:19:09 -0300

    adjust tests

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -4,27 +4,9 @@ class V1::UsersController < ApplicationController
     before_action :authenticate_devise_api_token!
     before_action :set_user, only: %i[ show update destroy ]
     
-    # GET /v1/users
-    def index
-        @users = User.all
-
-        render json: @users
-    end
-
     # GET /v1/users/:id
     def show
         render json: @user
-    end
-
-    # POST /v1/users
-    def create
-        @user = User.new(user_params)
-
-        if @user.save
-        render json: @user, status: :created
-        else
-        render json: @user.errors, status: :unprocessable_entity
-        end
     end
 
     # PATCH/PUT /v1/users/:id
@@ -34,11 +16,6 @@ class V1::UsersController < ApplicationController
         else
         render json: @user.errors, status: :unprocessable_entity
         end
-    end
-
-    # DELETE /v1/users/:id
-    def destroy
-        @user.destroy
     end
     
     private
--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -2,11 +2,7 @@ require "rails_helper"
 
 RSpec.describe V1::UsersController, type: :routing do
     context "Routes" do
-        it { is_expected.to route(:get, '/v1/users').to(action: :index) }
         it { is_expected.to route(:get, '/v1/users/1').to(action: :show, id: 1) }
-        it { is_expected.to route(:post, '/v1/users').to(action: :create) }
         it { is_expected.to route(:put, '/v1/users/1').to(action: :update, id: 1) }
-        it { is_expected.to route(:patch, '/v1/users/1').to(action: :update, id: 1) }
-        it { is_expected.to route(:delete, '/v1/users/1').to(action: :destroy, id: 1) }
     end
 end
